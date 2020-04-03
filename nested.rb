82f532486ec6c4dfa7839b5cb7b5bd34a1ae1b75
Bala Luguja <bluguja@gmail.com>
2020-04-03 15:37:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,8 +59,12 @@ def dennis_ritchies_language
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+     
+ 		}
+
+programmer_hash[:dennis_ritchie][:languages][0]
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
